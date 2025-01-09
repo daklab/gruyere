9f7841d3efa49aad6ad60fe3145ee59964000eb9
adas-7 <adas@pe2-login01.c.nygenome.org>
2025-01-09 14:19:12 -0500

    updating mapping

--- src/preprocess/map_annotations.py
+++ src/preprocess/map_annotations.py
@@ -12,11 +12,12 @@ warnings.filterwarnings('ignore')
 variant_type = sys.argv[1]  # "coding" or "noncoding"
 chromosome = int(sys.argv[2])
 
-VARIANTS_EXTRACT = f"/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/Alzheimer-RV/data/genotypes/to_extract/{variant_type}/ADSP.chr{chromosome}.txt"
+ABC_PATH = "/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/annotations/brain_all/glass_lab_fastq/processed_files/ABC_data"
+GENE_POS = "/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/Alzheimer-RV/data/gene_positions38_ensemble.txt"
 ANNOTATION_FILE_PATH = f"/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/36K_QC/WGSA_annotations/ADSP_annotated_chr{chromosome}.annotated.snp.gz"
 OUTPUT_DIR = f"/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/Alzheimer-RV/data/wgsa_annotated/{variant_type}"
 
-print(variant_type, chromosome, VARIANTS_EXTRACT, ANNOTATION_FILE_PATH)
+print(variant_type, chromosome, ABC_PATH, GENE_POS, ANNOTATION_FILE_PATH)
 
 coding_annotations = ['chr','pos','MAP20','phyloP17way_primate','phyloP30way_mammalian',
                      'phastCons30way_mammalian','phastCons17way_primate_rankscore', 'integrated_fitCons_score',
@@ -92,8 +93,15 @@ def make_coding_numerical(df):
     df = process_spliceai_column(df, ['SpliceAI_DS_AG', 'SpliceAI_DS_AL', 'SpliceAI_DS_DG', 'SpliceAI_DS_DL'])
     # make regulomedb quantitative
     for val in df['RegulomeDB_score'].unique():
-        new_val = float(re.search(r'\d+', str(val)).group())
-        df.loc[df["RegulomeDB_score"] == val, "RegulomeDB_score"] = new_val
+        match = re.search(r'\d+', str(val))
+        if match:
+            new_val = float(match.group())
+            df.loc[df["RegulomeDB_score"] == val, "RegulomeDB_score"] = new_val
+        else:
+            print(f"Warning: No numerical value found in 'RegulomeDB_score' for {val}.")
+            # Handle this case, e.g., by setting a default value or skipping
+            df.loc[df["RegulomeDB_score"] == val, "RegulomeDB_score"] = 0  # Set NaN or a default value
+
     df.loc[df['FANTOM5_CAGE_peak_robust']=='Y','FANTOM5_CAGE_peak_robust'] = 1
     df.loc[df['FANTOM5_CAGE_peak_robust']!=1,'FANTOM5_CAGE_peak_robust'] = 0
     df['BayesDel_addAF_score'] = df['BayesDel_addAF_score'].str.split("|").str[0] # keeping first val if many
@@ -160,12 +168,37 @@ def make_noncoding_numerical(df):
     return df
 
 
-def main():
-    to_extract = pd.read_csv(VARIANTS_EXTRACT, sep = "\t", header = None)
-    if variant_type == "coding": annotations = coding_annotations
-    if variant_type == "noncoding": annotations = noncoding_annotations
+def make_extract_coding():
+    gene_pos= pd.read_csv(GENE_POS, sep = "\t")
+    gene_pos = gene_pos[gene_pos['biotype']=="protein_coding"]
+    chr_pos = gene_pos[gene_pos['chr']==str(chromosome)]
+    chr_pos = chr_pos.drop_duplicates("ensgene")
+    chr_pos['Gene'] = np.where(chr_pos['symbol'].isna() == False, chr_pos['symbol'], chr_pos['ensgene'])
+    chr_pos = chr_pos[['chr','start','end','Gene']].drop_duplicates()
+    return chr_pos
 
-    output = os.path.join(OUTPUT_DIR, variant_type, f"WGSA_chr{chromosome}.csv")
+def make_extract_noncoding():
+    noncoding_df = pd.DataFrame()
+    for cell in ['microglia','oligodendrocyte','astrocyte','neuron']:
+        cell_df = pd.read_csv(os.path.join(ABC_PATH, f'ABC_results_{cell}/{cell}/Predictions/EnhancerPredictionsFull_threshold0.02_self_promoter.tsv'), sep = "\t")
+        cell_df = cell_df[['chr','start','end','TargetGene']]
+        cell_df['CHR'] = cell_df['chr'].str.split("chr").str[1]
+        noncoding_df = pd.concat((noncoding_df, cell_df))
+        chr_df = noncoding_df[noncoding_df['CHR']==str(chromosome)]
+        chr_df = chr_df.drop_duplicates(['CHR','start','end'])
+        chr_df = chr_df[['CHR','start','end','TargetGene']].drop_duplicates()
+    return chr_df
+
+
+def main():
+    if variant_type == "coding": 
+        annotations = coding_annotations
+        to_extract = make_extract_coding()
+    if variant_type == "noncoding": 
+        annotations = noncoding_annotations
+        to_extract = make_extract_noncoding()
+
+    output = os.path.join(OUTPUT_DIR, f"WGSA_chr{chromosome}.csv")
     if os.path.isfile(output):
         print(output, "already exists, continue on")
         return None
@@ -178,13 +211,13 @@ def main():
         df = df[(df['Eigen_coding_or_noncoding'] =='c') | (df['fathmm-XF_coding_or_noncoding']=='coding')]
         coding_anno = get_extracted_df(df, to_extract)
         coding_anno = make_coding_numerical(coding_anno)
-        coding_anno.to_csv(output)
+        coding_anno.drop_duplicates().to_csv(output)
         print("done with ", output)
     else:
         df = df[(df['Eigen_coding_or_noncoding'] =='n') | (df['fathmm-XF_coding_or_noncoding']=='noncoding')]
         noncoding_anno = get_extracted_df(df, to_extract)
         noncoding_anno = make_noncoding_numerical(noncoding_anno)
-        noncoding_anno.to_csv(output)
+        noncoding_anno.drop_duplicates().to_csv(output)
         print("done with ", output)
     return
 
